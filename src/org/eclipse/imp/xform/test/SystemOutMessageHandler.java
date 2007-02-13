343271d778f75b7cc4be7fee151d37e5b3e92f4f
Philippe Charles <pgc1@rice.edu>
2007-02-13 01:13:17 +0000

    Update to work with new LPG distribution on Sourceforge

--- src/org/eclipse/imp/xform/test/SystemOutMessageHandler.java
+++ src/org/eclipse/imp/xform/test/SystemOutMessageHandler.java
@@ -1,9 +1,15 @@
 package com.ibm.watson.safari.xform.test;
 
-import lpg.lpgjavaruntime.IMessageHandler;
+import lpg.javaruntime.IMessageHandler;
 
 public class SystemOutMessageHandler implements IMessageHandler {
-    public void handleMessage(int offset, int length, String message) {
+    public void handleMessage(int errorCode, int [] msgLocation, int[] errorLocation, String filename, String [] errorInfo) {
+        int offset = msgLocation[IMessageHandler.OFFSET_INDEX],
+            length = msgLocation[IMessageHandler.LENGTH_INDEX];
+        String message = "";
+        for (int i = 0; i < errorInfo.length; i++)
+            message += (errorInfo[i] + (i < errorInfo.length - 1 ? " " : ""));
+
 	System.out.println("[" + offset + ":" + length + "] " + message);
     }
 }
